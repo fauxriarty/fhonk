db603e5de3fce657c2dd2c9f3e64895515ce7de1
fauxriarty <aditya.chheda04@gmail.com>
2025-01-14 01:46:47 +0530

    fix final?

--- cmd/handlers/handlers.go
+++ cmd/handlers/handlers.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"fhonk/cmd/spotify"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -26,41 +25,31 @@ func corsMiddleware() gin.HandlerFunc {
 }
 
 func SpotifyLoginHandler(w http.ResponseWriter, r *http.Request) {
-	// a random state parameter
-	state := "random_generated_state" // replace with actual random state generation
-	authURL := spotify.GenerateAuthURL(state)
+	frontendCallback := r.URL.Query().Get("callback_url")
+	if frontendCallback == "" {
+		http.Error(w, "Missing callback URL", http.StatusBadRequest)
+		return
+	}
+
+	authURL := spotify.GenerateAuthURL(frontendCallback)
 	http.Redirect(w, r, authURL, http.StatusFound)
 }
 
 func SpotifyCallbackHandler(w http.ResponseWriter, r *http.Request) {
 	code := r.URL.Query().Get("code")
-	state := r.URL.Query().Get("state") // State parameter from the original request
+	frontendCallback := r.URL.Query().Get("state")
 
 	if code == "" {
 		http.Error(w, "Missing authorization code", http.StatusBadRequest)
 		return
 	}
 
-	tokenData, err := spotify.ExchangeCodeForToken(code)
+	tokens, err := spotify.ExchangeCodeForToken(code)
 	if err != nil {
 		http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	accessToken, ok := tokenData["access_token"].(string)
-	if !ok {
-		http.Error(w, "Failed to get access token", http.StatusInternalServerError)
-		return
-	}
-	refreshToken, ok := tokenData["refresh_token"].(string)
-	if !ok {
-		http.Error(w, "Failed to get refresh token", http.StatusInternalServerError)
-		return
-	}
-
-	// Construct the callback URL with access and refresh tokens
-	frontendCallbackURL := fmt.Sprintf("%s?access_token=%s&refresh_token=%s", state, accessToken, refreshToken)
-
-	// Redirect the user to the frontend callback URL
-	http.Redirect(w, r, frontendCallbackURL, http.StatusFound)
+	redirectURL := frontendCallback + "?access_token=" + tokens.AccessToken + "&refresh_token=" + tokens.RefreshToken
+	http.Redirect(w, r, redirectURL, http.StatusFound)
 }
